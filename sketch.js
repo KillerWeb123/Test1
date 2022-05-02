82bc6bee1302d9d03797d190d934b0c2ec8585ce
KillerWeb123 <104758665+KillerWeb123@users.noreply.github.com>
2022-05-02 18:28:57 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var ball;
+function setup() {
+  createCanvas(400,400);
+  ball=createSprite(30,30,30,30)
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites();
+  console.log("Hello");
+
+}
+
+
+
+
